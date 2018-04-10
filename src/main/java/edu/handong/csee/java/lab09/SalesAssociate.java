4a172542046eb3ae6364f96f73c36b212cf66c06
21700383 <21700383@handong.edu>
2018-04-11 00:30:18 +0900

    changed employee to salesman in comments and javadoc

--- src/main/java/edu/handong/csee/java/lab09/SalesAssociate.java
+++ src/main/java/edu/handong/csee/java/lab09/SalesAssociate.java
@@ -12,39 +12,39 @@ public class SalesAssociate { // declare class
 	private double sales; // declare sales as private double
 	
 	/**
-	 * SalesAssociate constructor which will build the data for employee's name and sales
+	 * SalesAssociate constructor which will build the data for salesman's name and sales
 	 */
 	public void SalesAssociate() { // declare method
 		this.name = null; // set employee's name as no value for now
-		this.sales = 0; // set employee's sales as 0 for now
+		this.sales = 0; // set salesman's sales as 0 for now
 	}
 	
 	/**
-	 * setName method which sets the employee's name as the name string received.
+	 * setName method which sets the salesman's name as the name string received.
 	 * @param name
 	 */
 	public void setName(String name) { // declare method
-		this.name = name; // set the name of employee to name string received
+		this.name = name; // set the name of salesman to name string received
 	}
 	/**
-	 * setSales method which sets the employee's sales as the sales double received.
+	 * setSales method which sets the salesman's sales as the sales double received.
 	 * @param sales
 	 */
 	public void setSales(double sales) { // declare method
-		this.sales = sales; // set the sales of employee to sales double received
+		this.sales = sales; // set the sales of salesman to sales double received
 	}
 	/**
-	 * getName method which returns the name string of the employee
+	 * getName method which returns the name string of the salesman
 	 * @return
 	 */
 	public String getName() { // declare method
-		return this.name; // return the employee's name
+		return this.name; // return the salesman's name
 	}
 	/**
-	 * getSales method which returns the sales double of the employee
+	 * getSales method which returns the sales double of the salesman
 	 * @return
 	 */
 	public double getSales() { //declare method
-		return this.sales; // return the employee's sales
+		return this.sales; // return the salesman's sales
 	}
 }
