75785273246211b0b9c1307a73b2d2da0c685f7e
21700383 <21700383@handong.edu>
2018-04-11 00:52:07 +0900

    SalesReport file with javadoc and one line comments

--- src/main/java/edu/handong/csee/java/lab09/SalesReporter.java
+++ src/main/java/edu/handong/csee/java/lab09/SalesReporter.java
@@ -8,85 +8,96 @@ import java.util.Scanner; // import scanner utility to get user input
  * @author Admin
  *
  */
-public class SalesReporter {
+public class SalesReporter { // declare class
 	
-	private double highestSales = 0;
-	private double averageSales = 0;
-	private  SalesAssociate[] team;
+	private double highestSales = 0; // set highestSales as private double and set the value as 0
+	private double averageSales = 0; // set averageSales as private double and set the value as 0
+	private  SalesAssociate[] team; // set team as array of private SalesAssociate constructor type
 	
-	public void getData() {
-		System.out.println("Enter the number of salesmen: ");
-		Scanner myKeyboard = new Scanner(System.in);
+	/**
+	 * getData method which tells the user to input the number of salesmen, then for that amount of salesmen,
+	 * makes the user to input the name and sales of each salesman and store them.
+	 */
+	public void getData() { // declare method
+		System.out.println("Enter the number of salesmen: "); // tell the user to input number of salesmen
+		Scanner myKeyboard = new Scanner(System.in); // to get input from user keyboard
 
-		int numOfSalesmen = myKeyboard.nextInt();
+		int numOfSalesmen = myKeyboard.nextInt(); // get the next integer input from the keyboard
 		
-		team = new SalesAssociate[numOfSalesmen];
+		team = new SalesAssociate[numOfSalesmen]; // create input number of arrays with SalesAssociate type and name it as team
 		
-		for(int i = 0; i < numOfSalesmen; i++) {
-			System.out.println("Enter data for associate number" + (i+1) + ":");
-			System.out.print("Name? ");
+		for(int i = 0; i < numOfSalesmen; i++) { // make a loop which repeats until reaching numOfSalesmen
+			System.out.println("Enter data for associate number" + (i+1) + ":"); // tell user to input data for nth salesman
+			System.out.print("Name? "); // tell user to input name
 			myKeyboard.nextLine(); // eats enter
-			String name = myKeyboard.nextLine();
-			System.out.print("Sales? ");
-			double sales = myKeyboard.nextDouble();
-			team[i] = new SalesAssociate();
-			team[i].setName(name);
-			team[i].setSales(sales);
+			String name = myKeyboard.nextLine(); // get user input for name as string
+			System.out.print("Sales? "); // tell user to input sales
+			double sales = myKeyboard.nextDouble(); // get user input for sales as double
+			team[i] = new SalesAssociate(); // construct salesman's data
+			team[i].setName(name); // set salesman's name from user input
+			team[i].setSales(sales); // set salesman's sales from user input
 			
 		}
 	}
-	
-	public void computeAverage() {
-		double sum = 0;
+	/**
+	 * computeAverage method which computes the average sales of salesmen as a whole team.
+	 */
+	public void computeAverage() { //declare method
+		double sum = 0; // declare sum as double and set it as 0;
 		
-		for(int i = 0; i < team.length; i++) {
-			sum = sum + team[i].getSales();
+		for(int i = 0; i < team.length; i++) { // make a loop which repeats until reaching the last member of team
+			sum = sum + team[i].getSales(); // equation for sum of entire sales from all salesmen
 		}
-		averageSales = sum / team.length;
+		averageSales = sum / team.length; // equation for average sale of all salesmen, stored as averageSales
 	}
-	
-	public void computeTightestSales() {
+	/**
+	 * computeTightestSales method which compute to find the salesman with highest sales.
+	 */
+	public void computeTightestSales() { // declare method
 		
-		double highestSales = 0;
+		double highestSales = 0; // declare local highestSales as double and set it as 0;
 		
-		for(int i = 0; i < team.length; i++) {
-			if(highestSales < team[i].getSales())
-				highestSales = team[i].getSales();
+		for(int i = 0; i < team.length; i++) { // make a loop which repeats until reaching the last member of team
+			if(highestSales < team[i].getSales()) // if the sales of the current salesman is higher than higestSales
+				highestSales = team[i].getSales(); // the salesman's sales is the new highestSales
 		}
 		
-		this.highestSales = highestSales;
+		this.highestSales = highestSales; // set the highestSales as the highest sales found.
 	}	
-	
+	/**
+	 * printResults method which will print the computed data of average sales, highest sales and
+	 * data of salesman with highest sales and data of rest of the salesmen.
+	 */
 	public void printResults() {
 		
-		System.out.println("Average sales per associate is $" + averageSales);
-		System.out.println("The highest sales figure is $" + highestSales);
+		System.out.println("Average sales per associate is $" + averageSales); // print the average sales
+		System.out.println("The highest sales figure is $" + highestSales);// print the highest sales
 		
-		System.out.println("Salesman who has the highest sales:");
-		for(int i = 0; i < team.length; i++) {
-			if(team[i].getSales() == highestSales) {
-				System.out.println("Name: " + team[i].getName());
-				System.out.println("Sales: " + team[i].getSales());
+		System.out.println("Salesman who has the highest sales:");  // print who has the highest sales
+		for(int i = 0; i < team.length; i++) { // loop which repeats until reaching the last member of team to find the person with highest sales
+			if(team[i].getSales() == highestSales) { // if the salesman's sales matches the highestSales
+				System.out.println("Name: " + team[i].getName()); // print the salesman's name
+				System.out.println("Sales: " + team[i].getSales()); // print the salesman's sales
 			}
 		}
 		
-		System.out.println("The rest performed as followed:");
-		for(int i = 0; i < team.length; i++) {
-			if(team[i].getSales() != highestSales) {
-				System.out.println("Name: " + team[i].getName());
-				System.out.println("Sales: " + team[i].getSales());
+		System.out.println("The rest performed as followed:"); // print for the rest of the salesmen
+		for(int i = 0; i < team.length; i++) { // loop which repeats until reaching the last member of team for the rest of the salesmen
+			if(team[i].getSales() != highestSales) { // if the salesman's sales doesn't match the highestSales
+				System.out.println("Name: " + team[i].getName()); // print the salesman's name
+				System.out.println("Sales: " + team[i].getSales()); // print the salesman's sales
 			}
 		}
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) { // declare static method
 		// TODO Auto-generated method stub
 
-		SalesReporter analyzer = new SalesReporter();
-		analyzer.getData();
-		analyzer.computeAverage();
-		analyzer.computeTightestSales();
-		analyzer.printResults();
+		SalesReporter analyzer = new SalesReporter(); // set analyzer to the structure of SalesReporter class
+		analyzer.getData(); // call getData in SalesReporter
+		analyzer.computeAverage(); // call computeAverage in SalesReporter
+		analyzer.computeTightestSales(); // call computeTighestSales in SalesReporter
+		analyzer.printResults(); // call printResults in SalesReporter
 		}
 
 }
