392ecd65e580d09fd8352d06cc32122871b353b7
21700383 <21700383@handong.edu>
2018-04-11 00:17:22 +0900

    SalesAssociate file with javadocs and one line comments

--- src/main/java/edu/handong/csee/java/lab09/SalesAssociate.java
+++ src/main/java/edu/handong/csee/java/lab09/SalesAssociate.java
@@ -6,29 +6,45 @@ package edu.handong.csee.java.lab09;
  * @author Admin
  *
  */
-public class SalesAssociate {
+public class SalesAssociate { // declare class
 	
-	private String name;
-	private double sales;
+	private String name; // declare name as private String
+	private double sales; // declare sales as private double
 	
-	public void SalesAssociate() {
-		this.name = null;
-		this.sales = 0;
+	/**
+	 * SalesAssociate constructor which will build the data for employee's name and sales
+	 */
+	public void SalesAssociate() { // declare method
+		this.name = null; // set employee's name as no value for now
+		this.sales = 0; // set employee's sales as 0 for now
 	}
 	
-	public void setName(String name) {
-		this.name = name;
+	/**
+	 * setName method which sets the employee's name as the name string received.
+	 * @param name
+	 */
+	public void setName(String name) { // declare method
+		this.name = name; // set the name of employee to name string received
 	}
-	
-	public void setSales(double sales) {
-		this.sales = sales;
+	/**
+	 * setSales method which sets the employee's sales as the sales double received.
+	 * @param sales
+	 */
+	public void setSales(double sales) { // declare method
+		this.sales = sales; // set the sales of employee to sales double received
 	}
-	
-	public String getName() {
-		return this.name;
+	/**
+	 * getName method which returns the name string of the employee
+	 * @return
+	 */
+	public String getName() { // declare method
+		return this.name; // return the employee's name
 	}
-	
-	public double getSales() {
-		return this.sales;
+	/**
+	 * getSales method which returns the sales double of the employee
+	 * @return
+	 */
+	public double getSales() { //declare method
+		return this.sales; // return the employee's sales
 	}
 }
